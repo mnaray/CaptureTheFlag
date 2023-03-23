631ff28817fb271f88b1ca9913c8f8c4c37bcc90
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-03-23 12:59:05 +0100

    add script to db entrypoint to run on startup

--- src/sql_injection/database/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Quelle für dieses Script:
-# https://stackoverflow.com/questions/37465864/docker-how-to-run-sql-file-in-an-image
-
-set -e
-# service mysql start
-mysql </mysql/profiles.sql
-# service mysql stop
